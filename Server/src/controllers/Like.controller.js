b12c8de3ec6381b0e65ce688682ac499b051cf27
Himanshu-Khairnar <himanshuk1205@gmail.com>
2025-02-02 21:10:13 +0530

    Created Like routes

--- Server/src/controllers/Like.controller.js
+++ Server/src/controllers/Like.controller.js
@@ -1,1 +1,63 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import LikeModel from "../models/Like.model.js";
 
+export const postLike = asyncHandler(async (res, req) => {
+  try {
+    const user = req.user;
+    const { authorId } = req.params;
+    const { postId, commentId } = req.body;
+    if (!postId || !commentId) throw ApiError(404, "All fields are required");
+    const findLike = await LikeModel.findOne({
+      $or: [postId, commentId, authorId, (userId = user._id)],
+    });
+    if (findLike) {
+      const updateLike = await LikeModel.findByIdAndUpdate(findLike._id, {
+        like: false,
+      });
+      return res
+        .status(200)
+        .json(new ApiResponse(200, updateLike, "Successfully updated like"));
+    }
+    const createLike = await LikeModel.create({
+      like: true,
+      authorId,
+      postId,
+      commentId,
+      userId: user._id,
+    });
+    return res
+      .status(200)
+      .json(new ApiResponse(200, createLike, "Successfully created like"));
+  } catch (error) {
+    console.log(error);
+    throw ApiError(500, "Like in liking");
+  }
+});
+
+// const likeSchema = new Schema(
+//   {
+//     authorId: {
+//       type: Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//   userId: {
+//       type: Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     postId: {
+//       type: Schema.Types.ObjectId,
+//       ref: "Post",
+//       required: true,
+//     },
+//     commentId: {
+//       type: Schema.Types.ObjectId,
+//       ref: "Comment",
+//       required: false,
+//     },
+//   },
+//   { timestamps: true }
+// );
--- Server/src/models/Like.model.js
+++ Server/src/models/Like.model.js
@@ -1,21 +1,34 @@
 import mongoose, { Schema } from "mongoose";
 
-const likeSchema = new Schema({
+const likeSchema = new Schema(
+  {
     authorId: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     postId: {
-        type: Schema.Types.ObjectId,
-        ref: "Post",
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
     },
     commentId: {
-        type: Schema.Types.ObjectId,
-        ref: "Comment",
-        required: false
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+      required: false,
+    },
+    like:{
+        type:Boolean,
+        required:true,
+        default:false
     }
-}, { timestamps: true })    
+  },
+  { timestamps: true }
+);    
 
 export default Like = mongoose.models.Like || mongoose.model("Like", likeSchema)
